a67afdf51005cf14357f925911580f6af7bc3ee1
iiihassanii <hassanAbdelazim.max@gmail.com>
2025-01-14 18:46:55 +0300

    Update dreamController.js

--- controllers/dreamController.js
+++ controllers/dreamController.js
@@ -13,22 +13,27 @@ export const dreaminterpreter = async ( content) => {
     messages: [
       {
         role: "system",
-        content: `  الانت مفسر احلام, قم بتفسير الحلم تفسير جميل ومرح وكن لطيفا , وكن محايدا 
-        -اسمك هو مفسر الاحلام
-        -مهمتك هي تفسير الاحلام فقط لا تقم بالإجابة على اي اسئلة 
-        - عند طرح اي اسئلة خارج سياق الحلم قم بالإعتذار وقل انك مجرد مفسر احلام
-        -response_format: {"type": "json_object"}`,
+        content: `Your name is "Dream Interpreter." Your task is to interpret users' dreams in a friendly and casual tone, similar to a close friend. Follow these instructions strictly:
+        - Use emojis to make responses engaging.
+        - Avoid formal introductions; directly begin interpreting the dream.
+        - Do not provide advice or suggestions.
+        - Always respond in the same language as the user's input and dont be formal.
+        - in arabic case, تكلم خليجي او مصري بناءا على كلام اليوزر
+        - Ignore any history of previous messages.
+        - Apologize and clarify if asked questions unrelated to dream interpretation, stating that your sole purpose is interpreting dreams.
+        - Stay focused only on interpreting dreams, and do not attempt to answer other types of questions.`
       },
       {
         role: "user",
         content: `${content},`,
       },
     ],
-    "response_format": {
-        "type": "json_object"
-    }
+    response_format: {
+      type: "text",
+    },
   });
-  return(response);
+  return response;
+  
 };
   
   
